b3ed7e924cc0be0a844388e74b9cb78b030da948
Deekshant Yadav <d33kshant@gmail.com>
2022-03-25 12:13:53 +0530

    Create upload route

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 require('dotenv').config()
 const express = require('express')
+const fileupload = require('express-fileupload')
+const path = require('path')
+const { v4: uuid4 } = require('uuid')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
+app.use(fileupload({ createParentPath: true }))
 
-app.get('/', (req, res) => {
-	res.json({
-		message: "Hello World!"
-	})
+app.use('/', express.static('public'))
+
+app.post('/upload', (req, res) => {
+	try {
+		if (!req.files) {
+			res.json({
+				error: "No file to upload."
+			})
+		} else {
+			const image = req.files['image']
+			const name = image.name
+			const extension = name.substring(name.indexOf('.'), name.length)
+
+			const new_name = `${uuid4()}${extension}`
+			image.mv(path.join(__dirname, path.join('files', new_name)))
+
+			res.json({
+				message: "File uploaded.",
+				name: new_name,
+				type: image.mimetype,
+				size: image.size
+			})
+		}
+	} catch (error) {
+		res.json({
+			error
+		})
+	}
 })
 
 app.listen(PORT, ()=>{
